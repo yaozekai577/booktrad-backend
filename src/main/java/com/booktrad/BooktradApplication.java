8ed05f802518e5c736099a79ceb17a0d36f817c9
yaozekai <2321593248@qq.com>
2025-12-13 10:51:29 +0800

    初始项目

--- src/main/java/com/booktrad/BooktradApplication.java
+++ src/main/java/com/booktrad/BooktradApplication.java
@@ -1,13 +1,1 @@
-package com.booktrad;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class BooktradApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(BooktradApplication.class, args);
-    }
-
-}
